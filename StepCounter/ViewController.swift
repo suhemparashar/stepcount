4c5541f17377b57b2f9c6ffddd89342278e98e9b
suhem.parashar <suhemparashar@gmail.com>
2023-06-05 18:07:29 +0530

    Initial Commit

---
+++ StepCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StepCounter
+//
+//  Created by suhemparashar on 05/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
